9eef9d86f21362acc0943b97425592d7a83149ca
Taffetaism <rishoukin@gmail.com>
2025-09-28 15:26:13 -0700

    2nd

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,12 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 import tailwindcss from '@tailwindcss/vite'
 
+// const repoName = 'vuehub'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools(), tailwindcss()],
+  base: "/vuehub/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
